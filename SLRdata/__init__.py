aed844d5a6add14b0c519959fb03ef57d132b1f7
Olli Wilkman <olli.wilkman@iki.fi>
2017-10-09 16:28:06 +0300

    Fix stuff so setup.py works

--- SLRdata/__init__.py
+++ SLRdata/__init__.py
@@ -4,5 +4,5 @@ __version__ = '0.1'
 __author__ = 'Olli Wilkman'
 __license__ = 'MIT'
 
-from parse_cdr import parse_CRD
-from parse_cpf import parse_CPF, Prediction
+from .parse_crd import parse_CRD
+from .parse_cpf import parse_CPF, Prediction
--- setup.py
+++ setup.py
@@ -2,28 +2,22 @@
 
 import os
 import sys
-import my_package 
+import SLRdata 
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-requires = ["requests >= 1.0.0"]
 
 setup(
     name='SLRdata',
-    version = my_package.__version__,
+    version = SLRdata.__version__,
     description='SLR file format parsers',
     long_description=open('README.rst').read(),
     author='Olli Wilkman',
     author_email='olli.wilkman@iki.fi',
     packages=['SLRdata'],
-    install_requires=requires,
     license=open('LICENSE').read(),
-    zip_safe=False,
-    entry_points = {
-        # the script will be called runit
-        'console_scripts': ['runit = my_package.myfile:main',]
-        },
+    zip_safe=False
 )
